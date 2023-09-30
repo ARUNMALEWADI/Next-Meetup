f453d9a4d99aceefa907e90f2bcda796f2079724
arunmalewadi@gmail.com <arunmalewadi@gmail.com>
2023-09-30 15:41:33 +0530

    Prerendering the powers of Next JS

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,3 +12,50 @@ function MeetupDetails(){
 }
 
 export default MeetupDetails;
+
+export async function getStaticPaths(){
+
+
+return {
+  fallback:false,
+  paths:[
+    {
+params:{
+  meetupId:"m1",
+
+},
+params:{
+  meetupId:"m2",
+
+},
+params:{
+  meetupId:"m3",
+
+}
+
+    }
+  ]
+
+}
+
+}
+
+export async function getStaticProps(context){
+  const meetupId=context.params.meetupId;
+  console.log(meetupId)
+
+  return {
+    props:{
+      MeetupData:{
+        image:'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+        id:meetupId,
+        title:"First Meetup",
+        address:"some street 5",
+        description:"This is first meetup",
+      }
+      
+    }
+  }
+
+
+}
